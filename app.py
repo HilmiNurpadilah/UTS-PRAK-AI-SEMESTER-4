31698f24e33138c882f8c31c2e2e81a8a8945fdc
HilmiNurpadilah <158576356+HilmiNurpadilah@users.noreply.github.com>
2025-05-03 22:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -122,10 +122,8 @@ def results_file(filename):
 
 import os
 
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # GUNAKAN PORT DARI ENV
+    port = int(os.environ.get("PORT") or 8080)
     app.run(host='0.0.0.0', port=port)
 
 
